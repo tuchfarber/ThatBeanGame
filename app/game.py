06012bebda1d2a5b131610d2bc7ecae57633e4e7
Matt Tuchfarber <matt@tuchfarber.com>
2017-08-30 23:04:56 -0400

    Fix playing cards from empty hand

--- app/game.py
+++ app/game.py
@@ -200,6 +200,11 @@ class Game:
 
     def go_next_stage(self) -> None:
         self.stage_index = (self.stage_index + 1) % len(constants.STAGES)
+        current_player = self.player[self.current_player_index]
+        # If the player has an empty hand and is expected to play from hand, skip to next phase.
+        if not current_player.hand and self.stage_index in ['First Card', 'Second Card']:
+            go_next_stage()
+        return
 
     def go_next_player(self) -> None:
         self.current_player_index = (self.current_player_index + 1) % len(self.players)
