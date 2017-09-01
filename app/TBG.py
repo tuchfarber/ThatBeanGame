8a5c370cb793f09e87f51d8d310376ec09a50b72
Matt Tuchfarber <matt@tuchfarber.com>
2017-08-31 21:23:12 -0400

    Complete changeover to flask

--- app/TBG.py
+++ app/TBG.py
@@ -1,7 +1,7 @@
 from gevent.pywsgi import WSGIServer
 from geventwebsocket import WebSocketError
 from geventwebsocket.handler import WebSocketHandler
-#from bottle import request, Bottle, abort, response, route, post, run, static_file
+from functools import wraps
 import json
 from typing import Dict, List
 
@@ -20,6 +20,7 @@ clients: Dict[str, str] = {}
 
 def check_valid_request(f):
     '''Decorator. Verifies game exists and client is authorized. Returns game and client'''
+    @wraps(f)
     def wrapper(*args, **kwargs):
         try:
             game: Game = games[kwargs['game_id']]
@@ -56,11 +57,6 @@ def enable_cors(response):
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, user-agent'
     return response
 
-# @app.route('/<:re:.*>', methods=['OPTIONS')
-# def enable_options():
-#     '''Enables clients to receive okay from OPTIONS request'''
-#     return
-
 @app.route('/api/access', methods=['GET'])
 def access() -> Dict:
     '''
@@ -78,45 +74,44 @@ def access() -> Dict:
     return jsonify({'game': game_id, "player_name": player.name})
 
 
-# @app.route('/api/login', methods=['POST')
-# def login() -> Dict:
-#     '''
-#     Adds user to the requested game as long as game isn't full or already started,
-#     game exists, and user name isn't already used in game.
-#     '''
+@app.route('/api/login', methods=['POST'])
+def login() -> Dict:
+    '''
+    Adds user to the requested game as long as game isn't full or already started,
+    game exists, and user name isn't already used in game.
+    '''
+    post_data: Dict = request.get_json()
+    try:
+        name: str = post_data['name']
+        game_id: str = post_data['game']
+    except KeyError:
+        abort(400, util.error('Incorrect JSON data'))
 
-#     try:
-#         name: str = request.json['name']
-#         game_id: str = request.json['game']
-#     except KeyError:
-#         abort(400, util.error('Incorrect JSON data'))
+    # Get first public game
+    game: Game = util.shrink([games[game_id] for game_id in games if games[game_id].game_type == 'public'])
 
-#     # Get first public game
-#     game: Game = util.shrink([games[game_id] for game_id in games if games[game_id].game_type == 'public'])
+    # If game game_id isn't a blank string, use that game
+    if game_id in games:
+        game = games[game_id]
 
-#     # If game game_id isn't a blank string, use that game
-#     if game_id in games:
-#         game = games[game_id]
+    if name in [player.name for player in game.players]:
+        abort(400, util.error('User already exists with that name'))
+    if game.status != 'Awaiting':
+        abort(400, util.error('Game has already started or ended'))
+    if game.is_full():
+        abort(400, util.error('Game is full'))
 
-#     if name in [player.name for player in game.players]:
-#         abort(400, util.error('User already exists with that name'))
-#     if game.status != 'Awaiting':
-#         abort(400, util.error('Game has already started or ended'))
-#     if game.is_full():
-#         abort(400, util.error('Game is full'))
-
-#     player: Player = Player(name)
-#     game.add_player(player)
-#     response.set_cookie('tbg_token', player.token, max_age=400)
-#     clients[player.token] = game_id
-#     return util.success('Successfully logged into game')
+    player: Player = Player(name)
+    game.add_player(player)
+    clients[player.token] = game_id
+    response = make_response(jsonify(util.success('Successfully logged into game')))
+    response.set_cookie('tbg_token', player.token, max_age=400)
+    return response
 
 @app.route('/api/create', methods=['POST'])
 def create_new_game():
     '''Creates new player and game, returns game id'''
     post_data: Dict = request.get_json()
-    print(post_data)
-    print(request.json)
     try:
         player: Player = Player(post_data['name'])
     except KeyError:
@@ -137,131 +132,126 @@ def create_new_game():
     response.set_cookie('tbg_token', player.token, max_age=400)
     return response
 
-# @app.route('/api/game/<game_id>', methods=['GET'])
-# @check_valid_request
-# def game_status(game: Game, player: Player) -> Dict:
-#     '''Returns all game info'''
-#     result: Dict = game.retrieve_game(player)
-#     error_check(result)
-#     return result
+@app.route('/api/game/<game_id>', methods=['GET'])
+@check_valid_request
+def game_status(game: Game, player: Player) -> Dict:
+    '''Returns all game info'''
+    result: Dict = game.retrieve_game(player)
+    error_check(result)
+    return jsonify(result)
 
 
-# @app.route('/api/game/<game_id>/start', methods=['POST')
-# @check_valid_request
-# def start_game(game: Game, player: Player) -> Dict:
-#     '''Starts requested game if user is host'''
-#     result: Dict = game.start_game(player)
-#     error_check(result)
-#     return result
+@app.route('/api/game/<game_id>/start', methods=['POST'])
+@check_valid_request
+def start_game(game: Game, player: Player) -> Dict:
+    '''Starts requested game if user is host'''
+    result: Dict = game.start_game(player)
+    error_check(result)
+    return jsonify(result)
 
 
-# @app.route('/api/game/<game_id>/play/hand', methods=['POST')
-# @check_valid_request
-# def play_card_from_hand(game: Game, player: Player) -> Dict:
-#     '''Plays top card from hand to field specified'''
-#     try:
-#         field_index: int = request.json['field_index']
-#     except KeyError:
-#         abort(400, util.error('Incorrect JSON data'))
-#     result: Dict = game.hand_to_field(player, field_index)
-#     error_check(result)
-#     return result
+@app.route('/api/game/<game_id>/play/hand', methods=['POST'])
+@check_valid_request
+def play_card_from_hand(game: Game, player: Player) -> Dict:
+    '''Plays top card from hand to field specified'''
+    post_data: Dict = request.get_json()
+    try:
+        field_index: int = post_data['field_index']
+    except KeyError:
+        abort(400, util.error('Incorrect JSON data'))
+    result: Dict = game.hand_to_field(player, field_index)
+    error_check(result)
+    return jsonify(result)
 
 
-# @app.route('/api/game/<game_id>/play/market', methods=['POST')
-# @check_valid_request
-# def play_card_from_market(game: Game, player: Player) -> Dict:
-#     '''Places card from market into field'''
-#     try:
-#         field_index: int = request.json['field_index']
-#         card_id: str = request.json['card_id']
-#     except KeyError:
-#         abort(400, util.error('Incorrect JSON data'))
-#     result: Dict = game.market_to_field(player, field_index, card_id)
-#     error_check(result)
-#     return result
+@app.route('/api/game/<game_id>/play/market', methods=['POST'])
+@check_valid_request
+def play_card_from_market(game: Game, player: Player) -> Dict:
+    '''Places card from market into field'''
+    post_data: Dict = request.get_json()
+    try:
+        field_index: int = post_data['field_index']
+        card_id: str = post_data['card_id']
+    except KeyError:
+        abort(400, util.error('Incorrect JSON data'))
+    result: Dict = game.market_to_field(player, field_index, card_id)
+    error_check(result)
+    return jsonify(result)
 
-# @app.route('/api/game/<game_id>/play/pending', methods=['POST')
-# @check_valid_request
-# def play_card_from_pending(game: Game, player: Player) -> Dict:
-#     '''Places card from market into field'''
-#     try:
-#         card_id: str = request.json['card_id']
-#         field_index: int = request.json['field_index']
-#     except KeyError:
-#         abort(400, util.error('Incorrect JSON data'))
-#     result: Dict = game.pending_to_field(player, field_index, card_id)
-#     error_check(result)
-#     return result
-
-
-# @app.route('/api/game/<game_id>/draw/market', methods=['POST')
-# @check_valid_request
-# def draw_for_market(game: Game, player: Player) -> Dict:
-#     '''Draws two cards and places them in market'''
-#     result: Dict = game.deck_to_market(player)
-#     error_check(result)
-#     return result
+@app.route('/api/game/<game_id>/play/pending', methods=['POST'])
+@check_valid_request
+def play_card_from_pending(game: Game, player: Player) -> Dict:
+    '''Places card from market into field'''
+    post_data: Dict = request.get_json()
+    try:
+        card_id: str = post_data['card_id']
+        field_index: int = post_data['field_index']
+    except KeyError:
+        abort(400, util.error('Incorrect JSON data'))
+    result: Dict = game.pending_to_field(player, field_index, card_id)
+    error_check(result)
+    return jsonify(result)
 
 
-# @app.route('/api/game/<game_id>/draw/hand', methods=['POST')
-# @check_valid_request
-# def draw_for_hand(game: Game, player: Player) -> Dict:
-#     '''Draws three cards and places them in players hand'''
-#     result: Dict = game.deck_to_hand(player)
-#     error_check(result)
-#     return result
+@app.route('/api/game/<game_id>/draw/market', methods=['POST'])
+@check_valid_request
+def draw_for_market(game: Game, player: Player) -> Dict:
+    '''Draws two cards and places them in market'''
+    result: Dict = game.deck_to_market(player)
+    error_check(result)
+    return jsonify(result)
 
-# @app.route('/api/game/<game_id>/trade/create', methods=['POST')
-# @check_valid_request
-# def create_trade(game: Game, player: Player) -> Dict:
-#     '''Creates new trade'''
-#     try:
-#         card_ids: List[str] = request.json['card_ids']
-#         other_player_name: str = request.json['other_player']
-#         wants: List[str] = request.json['wants'] # List of card names
-#     except KeyError:
-#         abort(400, util.error('Incorrect JSON data'))
 
-#     result = game.create_trade(player, other_player_name, card_ids, wants)
-#     error_check(result)
-#     return result
+@app.route('/api/game/<game_id>/draw/hand', methods=['POST'])
+@check_valid_request
+def draw_for_hand(game: Game, player: Player) -> Dict:
+    '''Draws three cards and places them in players hand'''
+    result: Dict = game.deck_to_hand(player)
+    error_check(result)
+    return jsonify(result)
 
-# @app.route('/api/game/<game_id>/trade/accept', methods=['POST')
-# @check_valid_request
-# def accept_trade(game: Game, player: Player) -> Dict:
-#     '''Accepts a trade'''
-#     try:
-#         trade_id: str = request.json['trade_id']
-#         card_ids: List[str] = request.json['card_ids']
-#     except KeyError:
-#         abort(400, util.error('Incorrect JSON data'))
-#     result = game.accept_trade(player, trade_id, card_ids)
-#     error_check(result)
-#     return result
+@app.route('/api/game/<game_id>/trade/create', methods=['POST'])
+@check_valid_request
+def create_trade(game: Game, player: Player) -> Dict:
+    '''Creates new trade'''
+    post_data: Dict = request.get_json()
+    try:
+        card_ids: List[str] = post_data['card_ids']
+        other_player_name: str = post_data['other_player']
+        wants: List[str] = post_data['wants'] # List of card names
+    except KeyError:
+        abort(400, util.error('Incorrect JSON data'))
 
-# @app.route('/api/game/<game_id>/trade/reject', methods=['POST')
-# @check_valid_request
-# def reject_trade(game: Game, player: Player) -> Dict:
-#     '''Rejects a trade'''
-#     try:
-#         trade_id: str = request.json['trade_id']
-#     except KeyError:
-#         abort(400, util.error('Incorrect JSON data'))
-#     result = game.reject_trade(player, trade_id)
-#     error_check(result)
-#     return result
+    result = game.create_trade(player, other_player_name, card_ids, wants)
+    error_check(result)
+    return jsonify(result)
 
-# @app.route('/', methods=['GET'])
-# def index():
-#     '''Returns webpage'''
-#     return static_file('tbg.html', './app/static')
+@app.route('/api/game/<game_id>/trade/accept', methods=['POST'])
+@check_valid_request
+def accept_trade(game: Game, player: Player) -> Dict:
+    '''Accepts a trade'''
+    post_data: Dict = request.get_json()
+    try:
+        trade_id: str = post_data['trade_id']
+        card_ids: List[str] = post_data['card_ids']
+    except KeyError:
+        abort(400, util.error('Incorrect JSON data'))
+    result = game.accept_trade(player, trade_id, card_ids)
+    error_check(result)
+    return jsonify(result)
 
-# @app.route('/<filename:path>', methods=['GET'])
-# def static_assets(filename):
-#     print(filename)
-#     '''Returns asset'''
-#     return static_file(filename, './app/static')
+@app.route('/api/game/<game_id>/trade/reject', methods=['POST'])
+@check_valid_request
+def reject_trade(game: Game, player: Player) -> Dict:
+    '''Rejects a trade'''
+    post_data: Dict = request.get_json()
+    try:
+        trade_id: str = post_data['trade_id']
+    except KeyError:
+        abort(400, util.error('Incorrect JSON data'))
+    result = game.reject_trade(player, trade_id)
+    error_check(result)
+    return jsonify(result)
 
 print("Server starting...")
 server = WSGIServer(('0.0.0.0', 8080), app, handler_class=WebSocketHandler)
